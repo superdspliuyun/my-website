6c80aedf87ea260cbb1e8b70c4d83730f86ad71d
superdspliuyun <266626175+superdspliuyun@users.noreply.github.com>
2026-09-19 16:18:36 +0800

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/yun-website/',
+  base: '/my-website/',
   plugins: [react(), tailwindcss()],
 })
